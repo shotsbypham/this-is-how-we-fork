3ce567618f86bdbb70270dcf527bc45cac816b1a
sheyna <sheynawatkins@gmail.com>
2022-07-14 02:13:59 -0700

    added app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let answer = "red";
+let valueToCheck = "orange";
+
+if (valueToCheck === answer) {
+  console.log('The values match!');
+} else {
+  console.log('The values don\'t match');
+}
